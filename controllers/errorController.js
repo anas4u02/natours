0fc0d73450b1b71038f0ee3d0e165f9a6f1d74e4
Mohammad Anas <anas4u02@gmail.com>
2024-01-24 17:50:15 +0530

    Segregated development and prod errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,37 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        error: err,
+        status: err.status,
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+const sendErrorProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        console.log("ERROR 🌋");
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong, please try again!'
+        });
+    }
+
+}
+
 module.exports = (err, req, res, next) => {
-    console.log(err.stack);
+
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-
+    if (process.env.NODE_ENV == 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV == 'production') {
+        sendErrorProd(err, res);
+    }
     next();
 }
