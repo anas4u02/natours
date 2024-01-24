55f0d97deb3183ded0e673849d982d83b5336c43
Mohammad Anas <anas4u02@gmail.com>
2024-01-24 12:27:37 +0530

    Made a middleware for efficient handling of errors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const userRouter = require('./routes/user-routes');
 const tourRouter = require('./routes/tour-routes');
+const e = require('express');
 
 const app = express();
 app.use(express.static(`${__dirname}/public`));
@@ -23,10 +24,31 @@ app.use((req, res, next) => {
 // The execution of code in nodejs is sequential, if the above part gets executed the code won't reach
 // here. If all the above routes fails, then the routes are handled below.
 app.all('*', (req, res, next)=> {
-  res.status(404).json({
-    status: 'fail',
-    message: "Can't find the requested route!"
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: "Can't find the requested route!"
+  // });
+
+  const err = new Error(`Can't find the requested ${req.originalUrl} route!`);
+  err.status= 'fail';
+  err.statusCode = 404;
+  err.message= err.message;
+
+  next(err);
+
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status =  err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
+
+  next();
+
 });
 
 // START SERVER
