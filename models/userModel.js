9304b264706eb2e605ccab14f27a0f2e3c111409
Mohammad Anas <anas4u02@gmail.com>
2024-05-28 00:25:47 +0530

    Added encryption for password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,24 @@ const userSchema = new mongoose.Schema({
   passwordConfirmation: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // Only works on CREATE and SAVE!
+      validator: function (v) {
+        return v === this.password;
+      }
+    }
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if(!this.isModified('password')) return next();
+
+  // Hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // We need the passwordConfirm field only for UX purposes, not to be persisted in DB
+  this.passwordConfirmation = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
